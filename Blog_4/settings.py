c94c3aaade77f39aa74313aaa09edbfb18dc436f
Tim_redBlack <67490655+timoluxinne@users.noreply.github.com>
2020-11-17 14:22:42 +0300

    Added Register, Login and Logout

--- Blog_4/settings.py
+++ Blog_4/settings.py
@@ -133,3 +133,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Account Created successfully! You can now login")
+            return redirect('/')
+    form = UserRegisterForm()
+    context= {
+        'form': form
+    }
+    return render(request, 'users/register.html', context)
