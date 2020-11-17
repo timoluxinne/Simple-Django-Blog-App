d8757ec8db0b06d4341dfe8e00a7be1c6633b00a
Tim_redBlack <67490655+timoluxinne@users.noreply.github.com>
2020-11-17 10:18:44 +0300

    Added Update and Delete Views
    Created Update View
    Created Delete View and added post_confirm_delete file
    Added links to post_detail for each

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostCreateView, PostDetailView, PostListView
+from .views import PostCreateView, PostDeleteView, PostDetailView, PostListView, PostUpdateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path('post/new', PostCreateView.as_view(), name='post-create'),
-    path('post/post-detail/<str:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post-detail/<str:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post-update/<str:pk>/', PostUpdateView.as_view(), name='post-update'),
+    path('post-delete/<str:pk>/', PostDeleteView.as_view(), name='post-delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -24,3 +24,31 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
