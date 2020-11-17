0ca260a0941815a4005f04150e59033054735d36
Tim_redBlack <67490655+timoluxinne@users.noreply.github.com>
2020-11-17 09:09:48 +0300

    Imported ClassBasedViews in views.py
    Class Based Views Imports
    Registered Post Model in admin.py
    Created a Class List View

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView,
+    UpdateView
+)
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/home.html'
